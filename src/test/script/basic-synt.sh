a063d9e8538461bf4da9d08f2bab68001dabd4b1
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-05-10 20:09:49 +0200

    add all *.deca tests to basic-synt.sh

--- src/test/script/basic-synt.sh
+++ src/test/script/basic-synt.sh
@@ -20,7 +20,7 @@ cd "$(dirname "$0")"/../../.. || exit 1
 PATH=./src/test/script/launchers:"$PATH"
 
 # exemple de définition d'une fonction
-test_synt_invalide () {
+test_synt_invalid () {
     # $1 = premier argument.
     if test_synt "$1" 2>&1 | grep -q -e "$1:[0-9][0-9]*:"
     then
@@ -29,19 +29,30 @@ test_synt_invalide () {
         echo "Succes inattendu de test_synt sur $1."
         exit 1
     fi
-}    
+}
+
+test_synt_valid () {
+    # $1 = premier argument.
+    if test_synt "$1" 2>&1 | grep -q -e "$1:[0-9][0-9]*:"
+    then
+        echo "Echec inattendu pour test_synt sur $1."
+        exit 1
+    else
+        echo "Succes attendu de test_synt sur $1."
+    fi
+}
 
 for cas_de_test in src/test/deca/syntax/invalid/provided/*.deca
 do
-    test_synt_invalide "$cas_de_test"
+    test_synt_invalid "$cas_de_test"
 done
 
+for cas_de_test in src/test/deca/syntax/valid/provided/*.deca
+do
+    test_synt_valid "$cas_de_test"
+done
 
-if test_synt src/test/deca/syntax/valid/hello.deca 2>&1 | \
-    grep -q -e ':[0-9][0-9]*:'
-then
-    echo "Echec inattendu pour test_synt"
-    exit 1
-else
-    echo "Succes attendu de test_synt"
-fi
+for cas_de_test in src/test/deca/syntax/valid/*.deca
+do
+    test_synt_valid "$cas_de_test"
+done
