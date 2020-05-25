fdfc9ccfe5d0a101c35e2b211c89360649c070ef
Elina Houde <elina.houde@grenoble-inp.fr>
2020-05-25 12:23:59 +0200

    Fichiers .expected Etape A (pour while) et Etape B

--- src/main/java/fr/ensimag/deca/tree/While.java
+++ src/main/java/fr/ensimag/deca/tree/While.java
@@ -43,6 +43,8 @@ public class While extends AbstractInst {
     protected void verifyInst(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass, Type returnType)
             throws ContextualError {
+        condition.verifyExpr(compiler, localEnv, currentClass);
+        body.verifyListInst(compiler, localEnv, currentClass, returnType);
     }
 
     @Override
