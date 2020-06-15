a5207b6b70069b2bd4842132b97548eb1b315fdb
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-06-15 17:03:54 +0200

    add package-info for unit tests packages

---
+++ src/test/java/fr/ensimag/deca/codegen/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Unit tests of utilities classes used for IMA code generation.
+ *
+ * Unit tests for IMA code generation methods are implemented in {@link fr.ensimag.deca.tree} package.
+ *
+ * @author Equipe GL2
+ * @date 2020
+ */
+package fr.ensimag.deca.codegen;
