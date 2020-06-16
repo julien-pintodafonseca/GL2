889a35bf0c5124b20d02617f227d33e7f0cfc45d
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-06-16 17:42:40 +0200

    TestPlus ok

--- src/test/java/fr/ensimag/deca/context/TestPlusAdvanced.java
+++ src/test/java/fr/ensimag/deca/context/TestPlusAdvanced.java
@@ -1,16 +1,16 @@
 package fr.ensimag.deca.context;
 
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.tree.AbstractExpr;
-import fr.ensimag.deca.tree.ConvFloat;
-import fr.ensimag.deca.tree.Plus;
+import fr.ensimag.deca.ErrorMessages;
+import fr.ensimag.deca.tree.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -23,11 +23,14 @@ import static org.mockito.Mockito.when;
 public class TestPlusAdvanced {
     private final Type INT = new IntType(null);
     private final Type FLOAT = new FloatType(null);
+    private final FloatLiteral anyFloatExpr = new FloatLiteral(-99999.999f);
+    private final StringLiteral stringExpr1 = new StringLiteral("bonsoir");
+    private final BooleanLiteral booleanExpr2 = new BooleanLiteral(true);
 
-    @Mock private AbstractExpr intexpr1;
-    @Mock private AbstractExpr intexpr2;
-    @Mock private AbstractExpr floatexpr1;
-    @Mock private AbstractExpr floatexpr2;
+    @Mock private AbstractExpr intExpr1;
+    @Mock private AbstractExpr intExpr2;
+    @Mock private AbstractExpr floatExpr1;
+    @Mock private AbstractExpr floatExpr2;
 
     private DecacCompiler compiler;
     
@@ -35,45 +38,103 @@ public class TestPlusAdvanced {
     public void setup() throws ContextualError {
         MockitoAnnotations.initMocks(this);
         compiler = new DecacCompiler(null, null);
-        when(intexpr1.verifyExpr(compiler, null, null)).thenReturn(INT);
-        when(intexpr2.verifyExpr(compiler, null, null)).thenReturn(INT);
-        when(floatexpr1.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
-        when(floatexpr2.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+        when(intExpr1.verifyExpr(compiler, null, null)).thenReturn(INT);
+        when(intExpr2.verifyExpr(compiler, null, null)).thenReturn(INT);
+        when(floatExpr1.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+        when(floatExpr2.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
     }
 
     @Test
     public void testIntInt() throws ContextualError {
-        Plus plus = new Plus(intexpr1, intexpr2);
+        Plus plus = new Plus(intExpr1, intExpr2);
         // check the result
         assertTrue(plus.verifyExpr(compiler, null, null).isInt());
+        // ConvFloat should not have been inserted
+        assertFalse(plus.getRightOperand() instanceof ConvFloat);
+        assertFalse(plus.getLeftOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-        verify(intexpr1).verifyExpr(compiler, null, null);
-        verify(intexpr2).verifyExpr(compiler, null, null);
+        verify(intExpr1).verifyExpr(compiler, null, null);
+        verify(intExpr2).verifyExpr(compiler, null, null);
     }
 
     @Test
     public void testIntFloat() throws ContextualError {
-        Plus plus = new Plus(intexpr1, floatexpr1);
+        Plus plus = new Plus(intExpr1, floatExpr2);
         // check the result
         assertTrue(plus.verifyExpr(compiler, null, null).isFloat());
         // ConvFloat should have been inserted on the right side
         assertTrue(plus.getLeftOperand() instanceof ConvFloat);
         assertFalse(plus.getRightOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-        verify(intexpr1).verifyExpr(compiler, null, null);
-        verify(floatexpr1).verifyExpr(compiler, null, null);
+        verify(intExpr1).verifyExpr(compiler, null, null);
+        verify(floatExpr2).verifyExpr(compiler, null, null);
     }
 
     @Test
     public void testFloatInt() throws ContextualError {
-        Plus plus = new Plus(floatexpr1, intexpr1);
+        Plus plus = new Plus(floatExpr1, intExpr2);
         // check the result
         assertTrue(plus.verifyExpr(compiler, null, null).isFloat());
         // ConvFloat should have been inserted on the right side
         assertTrue(plus.getRightOperand() instanceof ConvFloat);
         assertFalse(plus.getLeftOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-        verify(intexpr1).verifyExpr(compiler, null, null);
-        verify(floatexpr1).verifyExpr(compiler, null, null);
+        verify(floatExpr1).verifyExpr(compiler, null, null);
+        verify(intExpr2).verifyExpr(compiler, null, null);
+    }
+
+    @Test
+    public void testFloatFloat() throws ContextualError {
+        Plus plus = new Plus(floatExpr1, floatExpr2);
+        // check the result
+        assertTrue(plus.verifyExpr(compiler, null, null).isFloat());
+        // ConvFloat should not have been inserted
+        assertFalse(plus.getRightOperand() instanceof ConvFloat);
+        assertFalse(plus.getLeftOperand() instanceof ConvFloat);
+        // check that the mocks have been called properly.
+        verify(floatExpr1).verifyExpr(compiler, null, null);
+        verify(floatExpr2).verifyExpr(compiler, null, null);
+    }
+
+    @Test
+    public void testWrongTypes() throws ContextualError {
+        // check that verifyExpr with a STRING leftOperand throws contextualError
+        Plus plus1 = new Plus(stringExpr1, anyFloatExpr);
+
+        Type tA1 = stringExpr1.verifyExpr(compiler, null, null);
+        Type tA2 = anyFloatExpr.verifyExpr(compiler, null, null);
+        assertTrue(tA1.isString());
+        assertTrue(tA2.isFloat());
+
+        ContextualError expected1 =
+                new ContextualError(ErrorMessages.CONTEXTUAL_ERROR_ARITHMETIC_OPERATION_INCOMPATIBLE_TYPE
+                        + tA1 + " (pour " + stringExpr1.decompile() + ") et " + tA2
+                        + " (pour " + anyFloatExpr.decompile() + ").", null);
+
+        ContextualError result1 = assertThrows(ContextualError.class, () -> {
+            plus1.verifyExpr(compiler, null, null);
+        });
+
+        assertThat(result1.getMessage(), is(expected1.getMessage()));
+
+
+        // check that verifyExpr with a BOOLEAN rightOperand throws contextualError
+        Plus plus2 = new Plus(anyFloatExpr, booleanExpr2);
+
+        Type tB1 = anyFloatExpr.verifyExpr(compiler, null, null);
+        Type tB2 = booleanExpr2.verifyExpr(compiler, null, null);
+        assertTrue(tB1.isFloat());
+        assertTrue(tB2.isBoolean());
+
+        ContextualError expected2 =
+                new ContextualError(ErrorMessages.CONTEXTUAL_ERROR_ARITHMETIC_OPERATION_INCOMPATIBLE_TYPE
+                        + tB1 + " (pour " + anyFloatExpr.decompile() + ") et " + tB2
+                        + " (pour " + booleanExpr2.decompile() + ").", null);
+
+        ContextualError result2 = assertThrows(ContextualError.class, () -> {
+            plus2.verifyExpr(compiler, null, null);
+        });
+
+        assertThat(result2.getMessage(), is(expected2.getMessage()));
     }
 }
