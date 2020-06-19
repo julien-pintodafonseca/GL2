20307728522a2920090bf271bae3f325c96244c9
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-06-19 15:27:34 +0200

    add getMaxValue, getMinValue and update getExponent

---
+++ src/test/java/include/MathSimu.java
@@ -0,0 +1,85 @@
+package include;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Equipe GL2
+ * @date 2020
+ */
+public class MathSimu {
+
+    public static void main(String[] args)  {
+        getMaxValueSimu();
+        getMinValueSimu();
+        getExponentSimu();
+        absSimu();
+        powSimu();
+    }
+
+    private static void getMaxValueSimu() {
+        System.out.println("float getMaxValue() :");
+        System.out.println(String.format("%6.5e",Float.MAX_VALUE));
+        System.out.println("--------------------");
+    }
+
+    private static void getMinValueSimu() {
+        System.out.println("float getMinValue() :");
+        System.out.println(String.format("%6.5e",Float.MIN_VALUE));
+        System.out.println("--------------------");
+    }
+
+    private static void getExponentSimu() {
+        System.out.println("int getExponent(float x) :");
+        // lot de valeurs
+        List<Float> lf = new ArrayList<>();
+        lf.add(1.0f);
+        lf.add(-1.0f);
+        lf.add(1.5f);
+        lf.add(-1.5f);
+        lf.add(2.0f);
+        lf.add(-2.0f);
+        lf.add(16.22f);
+        lf.add(-16.22f);
+        lf.add(27.39f);
+        lf.add(-27.39f);
+        lf.add(275467.7f);
+        lf.add(-275467.7f);
+        lf.add(0.0f);
+        lf.add(Float.MAX_VALUE);
+        lf.add(Float.MIN_VALUE);
+
+        for (Float f : lf) {
+            // on applique notre fonction
+            int val = java.lang.Math.getExponent(f);
+
+            System.out.println(val);
+        }
+        System.out.println("--------------------");
+    }
+
+    private static void absSimu() {
+        System.out.println("float abs(float x) :");
+        // lot de valeurs
+        List<Float> lf = new ArrayList<>();
+        lf.add(16f);
+        lf.add(-3f);
+        lf.add(134.4f);
+        lf.add(-246.22221f);
+
+        for (Float f : lf) {
+            // on applique notre fonction
+            Float val = java.lang.Math.abs(f);
+
+            System.out.println(String.format("%6.5e",val));
+        }
+        System.out.println("--------------------");
+    }
+
+    private static void powSimu() {
+        System.out.println("float pow(float x, int e) :");
+        // TODO
+        System.out.println("--------------------");
+    }
+}
