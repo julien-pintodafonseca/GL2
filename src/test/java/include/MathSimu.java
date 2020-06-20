c9d20d65b7a11c8d44265eda9d89951fbe8c26fe
Julien Pinto Da Fonseca <julien.pintodafonseca@gmail.com>
2020-06-20 20:15:32 +0200

    fix degrees reduction for negative number, add fact() and improve sin()

--- src/test/java/include/MathSimu.java
+++ src/test/java/include/MathSimu.java
@@ -156,6 +156,7 @@ public class MathSimu {
         lf.add(-((float)Math.PI*1.5f));             // -270°
         lf.add(-((float)Math.PI+(float)Math.PI/3)); // -330°
         lf.add(Integer.MAX_VALUE+256f);
+        lf.add(-Integer.MAX_VALUE+256f);
 
         for (Float f : lf) {
             // on applique notre fonction
